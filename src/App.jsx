65ec875ef0bd65f75afeb8f73cffba5d96331b4b
Rodrigo <rafigueroapacheco26@gmail.com>
2026-06-16 09:34:22 +0200

    setup incial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <main className="container">
+        <h1>React Netlify Github</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
